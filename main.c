9c694cd68aeefde5e15baa7b38b26d16c8299d92
Nikita Mikhaylov <darks1d3rr@gmail.com>
2019-09-27 01:06:26 +0300

    LED test + button interrupt

--- main.c
+++ main.c
@@ -4,8 +4,13 @@
 void gpt3cb(GPTDriver *gptp)
 {
     palTogglePad(GPIOB, GPIOB_LED1);
+}
+
+static void extcbutton(EXTDriver *extp, expchannel_t channel)
+{
+    (void)extp;
+    (void)channel;
     palTogglePad(GPIOB, GPIOB_LED2);
-    palTogglePad(GPIOB, GPIOB_LED3);
 }
 
 GPTConfig gpt3conf =
@@ -14,6 +19,28 @@ GPTConfig gpt3conf =
   .callback = gpt3cb,
   .cr2 = 0,
   .dier = 0
+};
+
+static const EXTConfig extconf = {
+  .channels =
+  {
+    [0]  = {EXT_CH_MODE_DISABLED, NULL},
+    [1]  = {EXT_CH_MODE_DISABLED, NULL},
+    [2]  = {EXT_CH_MODE_DISABLED, NULL},
+    [3]  = {EXT_CH_MODE_DISABLED, NULL},
+    [4]  = {EXT_CH_MODE_DISABLED, NULL},
+    [5]  = {EXT_CH_MODE_DISABLED, NULL},
+    [6]  = {EXT_CH_MODE_DISABLED, NULL},
+    [7]  = {EXT_CH_MODE_DISABLED, NULL},
+    [8]  = {EXT_CH_MODE_DISABLED, NULL},
+    [9]  = {EXT_CH_MODE_DISABLED, NULL},
+    [10] = {EXT_CH_MODE_DISABLED, NULL},
+    [11] = {EXT_CH_MODE_DISABLED, NULL},
+    [12] = {EXT_CH_MODE_DISABLED, NULL},
+    [13] = {EXT_CH_MODE_RISING_EDGE | EXT_CH_MODE_AUTOSTART | EXT_MODE_GPIOC, extcbutton},
+    [14] = {EXT_CH_MODE_DISABLED, NULL},
+    [15] = {EXT_CH_MODE_DISABLED, NULL},
+  }
 };
 
 int main(void)
@@ -40,6 +67,8 @@ int main(void)
 
     gptStart(&GPTD3, &gpt3conf);
     gptStartContinuous(&GPTD3, gpt3conf.frequency/4);
+
+    extStart (&EXTD1, &extconf);
 
     while (true)
     {
