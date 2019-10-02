1f181784531267c07e06caba56e8a6f90654e7f2
Nikita Mikhaylov <darks1d3rr@gmail.com>
2019-10-02 22:58:50 +0300

    LEDs PWM

--- main.c
+++ main.c
@@ -1,50 +1,23 @@
 #include <ch.h>
 #include <hal.h>
 
-void gpt3cb(GPTDriver *gptp)
-{
-    palTogglePad(GPIOB, GPIOB_LED1);
-}
-
-static void extcbutton(EXTDriver *extp, expchannel_t channel)
-{
-    (void)extp;
-    (void)channel;
-    palTogglePad(GPIOB, GPIOB_LED2);
-}
-
-GPTConfig gpt3conf =
-{
-  .frequency = 10000,
-  .callback = gpt3cb,
-  .cr2 = 0,
-  .dier = 0
-};
-
-static const EXTConfig extconf = {
-  .channels =
-  {
-    [0]  = {EXT_CH_MODE_DISABLED, NULL},
-    [1]  = {EXT_CH_MODE_DISABLED, NULL},
-    [2]  = {EXT_CH_MODE_DISABLED, NULL},
-    [3]  = {EXT_CH_MODE_DISABLED, NULL},
-    [4]  = {EXT_CH_MODE_DISABLED, NULL},
-    [5]  = {EXT_CH_MODE_DISABLED, NULL},
-    [6]  = {EXT_CH_MODE_DISABLED, NULL},
-    [7]  = {EXT_CH_MODE_DISABLED, NULL},
-    [8]  = {EXT_CH_MODE_DISABLED, NULL},
-    [9]  = {EXT_CH_MODE_DISABLED, NULL},
-    [10] = {EXT_CH_MODE_DISABLED, NULL},
-    [11] = {EXT_CH_MODE_DISABLED, NULL},
-    [12] = {EXT_CH_MODE_DISABLED, NULL},
-    [13] = {EXT_CH_MODE_RISING_EDGE | EXT_CH_MODE_AUTOSTART | EXT_MODE_GPIOC, extcbutton},
-    [14] = {EXT_CH_MODE_DISABLED, NULL},
-    [15] = {EXT_CH_MODE_DISABLED, NULL},
-  }
+static PWMConfig pwmcfg = {
+    .frequency = 70000,
+    .period = 1000,
+    .callback = NULL,
+    .channels = {
+        {.mode = PWM_OUTPUT_DISABLED, .callback = NULL},
+        {.mode = PWM_COMPLEMENTARY_OUTPUT_ACTIVE_HIGH, .callback = NULL},
+        {.mode = PWM_OUTPUT_DISABLED, .callback = NULL},
+        {.mode = PWM_OUTPUT_DISABLED, .callback = NULL}
+    },
+    .cr2 = 0,
+    .dier = 0
 };
 
 int main(void)
 {
+    char i = 0;
     /*
      * System initializations.
      * - HAL initialization, this also initializes the configured device drivers
@@ -55,23 +28,32 @@ int main(void)
     halInit();
     chSysInit();
 
-    palSetPad(GPIOB, GPIOB_LED1);
-    chThdSleepMilliseconds(200);
-    palClearPad(GPIOB, GPIOB_LED1);
-    palSetPad(GPIOB, GPIOB_LED2);
-    chThdSleepMilliseconds(200);
-    palClearPad(GPIOB, GPIOB_LED2);
-    palSetPad(GPIOB, GPIOB_LED3);
-    chThdSleepMilliseconds(200);
-    palClearPad(GPIOB, GPIOB_LED3);
-
-    gptStart(&GPTD3, &gpt3conf);
-    gptStartContinuous(&GPTD3, gpt3conf.frequency/4);
-
-    extStart (&EXTD1, &extconf);
+    palSetPadMode(GPIOB, GPIOB_LED1, PAL_MODE_ALTERNATE(1));
+    pwmStart(&PWMD1, &pwmcfg);
 
     while (true)
     {
-
+        while (true)
+        {
+            pwmEnableChannel(&PWMD1, 1, PWM_PERCENTAGE_TO_WIDTH(&PWMD1, i*1000));
+            chThdSleepMilliseconds(100);
+            i++;
+            if(i >= 10)
+            {
+                i = 10;
+                break;
+            }
+        }
+        while (true)
+        {
+           pwmEnableChannel(&PWMD1, 1, PWM_PERCENTAGE_TO_WIDTH(&PWMD1, i*1000));
+           chThdSleepMilliseconds(100);
+           i--;
+           if(i <= 0)
+           {
+               i = 0;
+               break;
+           }
+        }
     }
 }
